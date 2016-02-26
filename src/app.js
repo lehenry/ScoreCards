2b2e2a0b51c9d4a08d587550eefe4f8df54a1578
lehenry <loic.herve.mail@gmail.com>
2016-02-26 14:21:42 +0100

    Sending Players Names (ordered) in config page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 /**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
+ * ScoreCards Application
  */
 var currentPlayer=-1;
 var currentRound=0;
@@ -12,27 +10,38 @@ var players=[];
 var UI = require('ui');
 var Vector2 = require('vector2');
 var Settings = require('settings');
-console.log('Local: '+localStorage.getItem('playerNames'));
+
+
 var playersNames = [];
+
+//table colors for full scores
 var bgColor=[['#FFAAAA','#AAFFFF'],['#FF5555','#55FFFF']];
 if(Pebble.getActiveWatchInfo){
 var watch = Pebble.getActiveWatchInfo();
+  //no colors for aplite
   if(watch.platform=='aplite'){
     bgColor=[['white','white'],['white','white']];
   }
 }
 
-
+//unserialize stored players
 if(localStorage.getItem('playerNames')){
   try {
+   console.log('Local: '+JSON.parse(localStorage.getItem('playerNames')));
    playersNames = JSON.parse(localStorage.getItem('playerNames'));
   } catch(err){
     console.log(err);
   }
 }
+
 // Load configuration
 Settings.config(
   { url: 'http://lehenry.github.io/index.html' },
+  function(e) {
+    console.log('opening configurable');
+    // Reset the setting page player order on opening config
+    Settings.option('players', JSON.stringify(playersNames));
+  },  
   function(e) {
     console.log('closed configurable');
     // Show the parsed response
@@ -48,7 +57,7 @@ Settings.config(
 
     // Show the raw response if parsing failed
     if (e.failed) {
-      console.log(e.response);
+      //console.log(e.response);
     }
   }
 );
@@ -99,24 +108,32 @@ var PlayerCard=function(playerName,color){
   this.wind.add(this.topScore);
   
   this.wind.on('click', 'select', function(e) {
+    //select button goes to next player
     nextPlayer();
   });
   this.wind.on('longClick', 'select', function(e) {
+    //long click on select goes back to previous player
     previousPlayer();
   });  
   this.wind.on('click', 'up', function(e) {
+    //increment
     players[currentPlayer].addScore();
   });
   this.wind.on('click', 'down', function(e) {
+    //decrement
     players[currentPlayer].minusScore();
   });
   this.wind.on('longClick', 'up', function(e) {
+    //large increment
     players[currentPlayer].add5Score();
   });
   this.wind.on('longClick', 'down', function(e) {
+    //large decrement
     players[currentPlayer].minus5Score();
   });
 };
+
+//update the score and top score
 PlayerCard.prototype.displayScore=function(score,roundScore){
   this.scoreBox.text(score+"");
   this.roundScoreBox.text("Round #"+(currentRound+1)+": "+roundScore+"");
@@ -204,6 +221,7 @@ function init(){
 }
 init();
 
+//main menu
 var menu = new UI.Menu({
   sections: [{
     title: "ScoreCards",
@@ -222,11 +240,13 @@ var menu = new UI.Menu({
   
 menu.on('select', function(e) {
   if(e.itemIndex===0){
-      //new Game
+    //new Game - next selected menu entry is "resume"
     e.itemIndex=3;
     newGame();
   } else if (e.itemIndex==1){
+    //"Set Players" menu
     var menuPlayers = new UI.Menu();
+    
     for(var i=0;i<playersNames.length;i++){
       menuPlayers.item(0,i,
         {title:players[i].playerName, 
@@ -234,6 +254,7 @@ menu.on('select', function(e) {
         });
     }
     menuPlayers.show();
+    //on click change status active/inactive
     menuPlayers.on('select',function(ev){
       players[ev.itemIndex].active=!(players[ev.itemIndex].active);
       menuPlayers.item(0,ev.itemIndex,
@@ -241,12 +262,17 @@ menu.on('select', function(e) {
          subtitle:(players[ev.itemIndex].active)?"Active":"Inactive"
         });
     });
+    //on long click, selected player becomes first player
     menuPlayers.on('longSelect', function(e) {
       console.log(e.itemIndex);
       for(var i=0;i<e.itemIndex;i++){
         var last=players.shift();
         players.push(last);
+        var lastName=playersNames.shift();
+        playersNames.push(lastName);
       }
+      //save player order
+      localStorage.setItem('playerNames', JSON.stringify(playersNames));
       for(var j=0;j<playersNames.length;j++){
         menuPlayers.item(0,j,
           {title:players[j].playerName, 
@@ -256,8 +282,10 @@ menu.on('select', function(e) {
       menuPlayers.selection(0,0);
     });
   } else if (e.itemIndex==2){
+    //display... scores
     displayScores();
   } else if (e.itemIndex==3){
+    //resume to current player (or start new game)
     if(currentPlayer==-1){
       nextPlayer();
     }else{
@@ -271,7 +299,7 @@ menu.on('select', function(e) {
         
 menu.show();
 
-
+// init, start new game
 function newGame(){
   for(var j=0;j<players.length;j++){
     players[j].score=0;
@@ -282,63 +310,79 @@ function newGame(){
   }
 }
 
+// next player
 function nextPlayer(){
   var previousPlayer=currentPlayer;
   currentPlayer++;
-
+  //if previous player was last player, new round, back to first player
   if(currentPlayer>=players.length){
     currentPlayer=0;
     currentRound++;
   }
   console.log("Player "+currentPlayer+" of "+ players.length);
   console.log(players[currentPlayer].playerName+" "+players[currentPlayer].active);
+  
+  // show next player
   if(players[currentPlayer].active){
+    // init round
     if(typeof players[currentPlayer].roundScore[currentRound]!== 'number'){
       players[currentPlayer].roundScore[currentRound]=0;
     }
     players[currentPlayer].displayScore();
     players[currentPlayer].show();
   } else {
+    //skip inactive players
     nextPlayer();
   }
+  // hide previous player (unless it's a new game) 
   if(previousPlayer>-1){
       players[previousPlayer].hide();
   }
 }
+
+// previous player
 function previousPlayer(){
   var previousPlayer=currentPlayer;
   currentPlayer--;
-
+  // current player was first player, go back to last player, previous round
   if(currentPlayer<0){
     currentPlayer=players.length-1;
     currentRound--;
   }
+  //show player if active
   if(currentRound>=0){
     if(players[currentPlayer].active){
       players[currentPlayer].show();
     } else {
+      //skip inactive players
       previousPlayer();
     }
   } else {
+    //current player was first, on first round, will go back to menu
     currentRound=0;
     currentPlayer=-1;
   }
   players[previousPlayer].hide();  
 }
 
+
+//display scores
 function displayScores(){
   var scoreCard=new UI.Window({scrollable: true,backgroundColor: 'white'});
+  //for each player, display its score
   for(var i=0;i<players.length;i++){
-       scoreCard.add(new UI.Text({
-          font: 'gothic-28-bold',
-          position: new Vector2(0,i*30),
-          size: new Vector2(72,30),
-          text:players[i].playerName,
-          textAlign:'left',
-          color:'black',
-          textOverflow:'fill',
-          borderColor:'black'
-        }));
+    //player name
+    scoreCard.add(new UI.Text({
+        font: 'gothic-28-bold',
+        position: new Vector2(0,i*30),
+        size: new Vector2(72,30),
+        text:players[i].playerName,
+        textAlign:'left',
+        color:'black',
+        textOverflow:'fill',
+        borderColor:'black'
+    }));
+    //player score
     scoreCard.add(new UI.Text({
           font: 'gothic-28',
           position: new Vector2(72,i*30),
@@ -348,20 +392,22 @@ function displayScores(){
           color:'black',
           textOverflow:'fill',
           borderColor:'black'
-        }));
+    }));
   }
   scoreCard.show();
+  //on select, switch to full scores
   scoreCard.on('click', 'select',function(e){
     displayFullScores();
   });
 }
 
+// display full scores (per round)
 function displayFullScores(){
-  
   var fullScoreCard=new UI.Window({scrollable: true,backgroundColor: 'white'});
-  //var scoreBody="";
+  //column length is window length divided by the number of players
   var l=144/(players.length);
   var h=18;
+  // header, players names
   for(var p=0;p<players.length;p++){
         var head=new UI.Text({
           font: 'gothic-14',
@@ -375,6 +421,7 @@ function displayFullScores(){
         });
         fullScoreCard.add(head);
     }
+  //for each round, players' round score
   for(var round=0;round<=currentRound;round++){
     for(var i=0;i<players.length;i++){
         fullScoreCard.add(new UI.Text({
@@ -386,29 +433,34 @@ function displayFullScores(){
           color:'black',
           textOverflow:'fill',
           borderColor:'black',
+          //color table rows
           backgroundColor: bgColor[i%2][round%2]
         }));
     }
   }
-    for(var j=0;j<players.length;j++){
-        fullScoreCard.add(new UI.Text({
-          font: 'gothic-14',
-          position: new Vector2(l*j,h*(currentRound+1)),
-          size: new Vector2(l,h),
-          text:players[j].score +" ",
-          textAlign:'right',
-          color:'white',
-          textOverflow:'fill',
-          borderColor:'white',
-          backgroundColor: 'black'
-        }));
-    }  
+  
+  //score results
+  for(var j=0;j<players.length;j++){
+      fullScoreCard.add(new UI.Text({
+        font: 'gothic-14',
+        position: new Vector2(l*j,h*(currentRound+1)),
+        size: new Vector2(l,h),
+        text:players[j].score +" ",
+        textAlign:'right',
+        color:'white',
+        textOverflow:'fill',
+        borderColor:'white',
+        backgroundColor: 'black'
+      }));
+  }  
   fullScoreCard.show();
+  //on click, back to simple scores
   fullScoreCard.on('click', 'select',function(e){
     fullScoreCard.hide();
   });
 }
 
+// get the current high score
 function getTopPlayer(){
   var max=players[0].score;
   var playerId=0;
